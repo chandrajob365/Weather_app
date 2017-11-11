3809338c391e221d23d23321480d68f3faf738be
Manish <chandrajob365@gmail.com>
2017-11-12 00:56:28 +0530

    Add feature to update data based on location entered

---
+++ public/script/main.js
@@ -0,0 +1,118 @@
+const APPKEY = '6d0e3f929541145218c53f14a59c7aa2'
+
+const initialWeatherForecast = {
+  name: 'Bangalore',
+  dt: 1510291800,
+  weather: [{id: 801, main: 'Cloudy'}],
+  main: {
+    temp: 23,
+    scale: '°F',
+    humidity: 55
+  },
+  wind: {
+    speed: 30,
+    scale: 'mps',
+    deg: '43°'
+  },
+  sys: {
+    country: 'IN',
+    sunrise: 1510274776,
+    sunset: 1510316436
+  }
+}
+
+const getFormatedDate = epoch => {
+  var dt = new Date(epoch * 1000)
+  console.log('epoch = ', epoch, ' dt = ', dt)
+  return (format) => {
+    return converter(dt.toString(), format)
+  }
+}
+
+const converter = (dt, format) => {
+  console.log('dt = ', dt)
+  switch (format) {
+    case 'lastUpdated':
+      return dt.slice(0, dt.length - 15)
+    case 'sunrise':
+    case 'sunset':
+      return dt.match(/(\d{2}:){2}\d{2}/)[0]
+    default:
+  }
+}
+
+const getIconClass = weatherId => {
+  console.log('<getIconClass> currentWeatherNature = ', weatherId)
+  if (weatherId >= 200 && weatherId <= 232) return 'thunderstorms'
+  if (weatherId >= 300 && weatherId <= 321) return 'scattered-showers'
+  if (weatherId >= 500 && weatherId <= 531) return 'rain'
+  if (weatherId >= 600 && weatherId <= 622) return 'snow'
+  if (weatherId >= 700 && weatherId <= 781) return 'fog'
+  if (weatherId === 800) return 'clear'
+  if (/80[1-4]/.test(weatherId)) return 'cloudy'
+}
+
+const populateUpdatedData = (data, location) => {
+  console.log('dataLastUpdated = ', getFormatedDate(data.dt)('lastUpdated'), ' data.dt = ', data.dt)
+  let currentLocation = data.name
+  let countryCode = data.sys.country
+  let dataLastUpdated = getFormatedDate(data.dt)('lastUpdated')
+  let currentWeatherNature = data.weather[0].main
+  let currentWeatherId = data.weather[0].id
+  let currentTempVal = Math.floor(data.main.temp)
+  let humidity = data.main.humidity
+  let windVal = data.wind.speed
+  let windScale = data.wind.scale || 'mps'
+  let windDirection = data.wind.deg
+  let sunrise = getFormatedDate(data.sys.sunrise)('sunrise')
+  let sunset = getFormatedDate(data.sys.sunset)('sunset')
+  let replacableValues = document.querySelector('.visual .description .icon').classList.value.split(' ').length > 1
+    ? document.querySelector('.visual .description .icon').classList.value.split(' ').splice(1).join(' ')
+    : ''
+  document.querySelector('.weather').removeAttribute('hidden')
+  document.querySelector('.place .location').textContent = currentLocation + ' , ' + countryCode
+  document.querySelector('.place .date').textContent = dataLastUpdated
+  document.querySelector('.nature').textContent = currentWeatherNature
+  if (replacableValues.length > 0) {
+    document.querySelector('.visual .description .icon').classList.replace(replacableValues, getIconClass(currentWeatherId))
+  } else {
+    document.querySelector('.visual .description .icon').classList.add(getIconClass(currentWeatherId))
+  }
+  document.querySelector('.description .temprature .value').textContent = currentTempVal
+  document.querySelector('.description .temprature .scale').textContent = '°F'
+  document.querySelector('.humidity').textContent = 'Humidity : ' + humidity
+  document.querySelector('.wind ').textContent = 'Wind: ' + windVal + ' ' + windScale + ' ' + windDirection + '°'
+  document.querySelector('.sunrise').textContent = 'Sunrise: ' + sunrise
+  document.querySelector('.sunset').textContent = 'Sunset: ' + sunset
+  document.querySelector('.loader').setAttribute('hidden', true)
+}
+
+const fetchForecast = location => {
+  console.log('<fetchForecast> location = ', location)
+  const URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=' + APPKEY + '&units=metric&units=imperial'
+  const request = new XMLHttpRequest()
+  request.onreadystatechange = () => {
+    if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+      let response = JSON.parse(request.response)
+      console.log('response = ', response)
+      populateUpdatedData(response, location)
+    } else {
+      populateUpdatedData(initialWeatherForecast, initialWeatherForecast.name)
+    }
+  }
+  request.open('GET', URL)
+  request.send()
+}
+
+const fetchData = () => {
+  let newLoc = document.getElementById('newLoc')
+  console.log('newLoc.length = ', newLoc.value.length)
+  if (newLoc.value.length > 0) fetchForecast(newLoc.value)
+  newLoc.value = ''
+  newLoc.focus()
+}
+
+const updateForeacst = () => {
+  var currentDate =  new Date().get
+}
+populateData(initialWeatherForecast)
