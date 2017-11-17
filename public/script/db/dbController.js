d69a82e050df66ea1a96441191f2b76bbec06447
Manish <chandrajob365@gmail.com>
2017-11-17 13:03:47 +0530

    Add call to check IDB before insertion

--- public/script/db/dbController.js
+++ public/script/db/dbController.js
@@ -1,20 +1,20 @@
 
 const idbKeyVal = {
   set (key, val) {
-    return dbPromise.then(db => {
+    return app.dbPromise.then(db => {
       const tx =  db.transaction('locations', 'readwrite')
-      tx.objectStore('locations').put(key, val)
+      tx.objectStore('locations').put(val, key)
       return tx.complete
     }).catch(err => console.log('Error in saving value'))
   },
-  getAll () {
-    return dbPromise.then(db => {
+  getAllLocations () {
+    return app.dbPromise.then(db => {
       return db.transaction('locations', 'readonly')
         .objectStore('locations').getAll()
     })
   },
   get (key) {
-    return dbPromise.then(db => {
+    return app.dbPromise.then(db => {
       return db.transaction('locations', 'readonly')
         .objectStore('locations')
         .get(key)
@@ -22,5 +22,12 @@ const idbKeyVal = {
   }
 }
 
-const getAllLocations = (cb) =>
-  idbKeyVal.getAll().then(locationsObjs => cb(locationsObjs))
+const chkAndInsertInIDB = (lat, lng) => {
+  idbKeyVal.get(lat)
+    .then(obj => {
+      if (!obj) {
+        app.availabelLocations.push({lat: lat, lng: lng})
+        idbKeyVal.set(lat, {lat: lat, lng: lng})
+      }
+    })
+}
