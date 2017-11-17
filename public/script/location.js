9986ee8480f25f41fdf9cddc7c53c9e447aef8dc
Manish <chandrajob365@gmail.com>
2017-11-17 13:06:00 +0530

    Add check for valid locations before fetching weather

--- public/script/location.js
+++ public/script/location.js
@@ -17,17 +17,22 @@ function initAutocomplete () {
 const fillInAddress = () => {
   let place = locator.autocomplete.getPlace()
   console.log('place = ', place)
-  console.log('place.name = ', place.name)
-  let lat = place.geometry.location.lat()
-  console.log('lat = ', lat)
-  let lng = place.geometry.location.lng()
+  console.log('place.location = ', place.geometry.location)
+  if (place && place.geometry.location) {
+    console.log('<fillInAddress> Call to fetchForecast...........')
+    let lat = place.geometry.location.lat()
+    let lng = place.geometry.location.lng()
+    chkAndInsertInIDB(lat, lng)
+    app.fetchForecast(lat, lng)
+  }
   document.getElementById('locSearch').value = ''
-  app.fetchForecast(lat, lng)
+  document.getElementById('locSearch').focus()
 }
 
 function getCurrentGeoLocation () {
   toggleLocationDetectorIcon()
   if ('geolocation' in navigator) {
+    console.log('Inside GEOLOC call.............')
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError)
   } else {
     // Do Something
@@ -36,10 +41,13 @@ function getCurrentGeoLocation () {
 
 const geoSuccess = position => {
   console.log('<geoSuccess> lat = ', position.coords.latitude, ' longitude = ', position.coords.longitude)
+  let lat = position.coords.latitude
+  let lng = position.coords.longitude
   locator.iconShouldVisible = true
   toggleLocationDetectorIcon()
   locator.icon.blur()
   app.fetchForecast(position.coords.latitude, position.coords.longitude)
+  chkAndInsertInIDB(lat, lng)
 }
 
 const geoError = () => {
